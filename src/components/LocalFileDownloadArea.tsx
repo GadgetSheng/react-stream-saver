4f981cd7e626c46bc7e5679a83f674b9d535694f
jqsheng <jqsheng@trip.com>
2023-11-03 15:33:48 +0800

    chore: 完善流式下载待研究

--- src/components/LocalFileDownloadArea.tsx
+++ src/components/LocalFileDownloadArea.tsx
@@ -1,5 +1,38 @@
-function LocalFileDownloadArea(props: any) {
-    return null;
+import { useEffect, useRef } from "react";
+import { createDownloadStream } from "../utils";
+
+function LocalFileDownloadArea() {
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.addEventListener('change', async (e: any) => {
+                const [file] = e.target.files!;
+                if (file) {
+                    const reader = file.stream().getReader();
+                    const writableStream = await createDownloadStream(file.name);
+                    const writable = writableStream.getWriter();
+                    const pump = async () => {
+                        console.log('读取本地文件数据')
+                        const { done, value } = await reader.read();
+                        if (done) return writable.close();
+                        console.log('向下载线程下入数据', value);
+                        await writable.write(value);
+                        pump();
+                    }
+                    pump();
+                }
+            })
+        }
+    }, []);
+    const onDownload = () => {
+        if (inputRef.current) {
+            inputRef.current.click()
+        }
+    }
+    return (<>
+        <button onClick={onDownload}>本地流式文件下载</button>
+        <input ref={inputRef} type="file" hidden />
+    </>)
 }
 
 export default LocalFileDownloadArea
--- src/components/LocalFileZipArea.tsx
+++ src/components/LocalFileZipArea.tsx
@@ -1,5 +1,87 @@
-function LocalFileZipArea(props: any) {
-    return null;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef } from "react";
+import * as fflate from "fflate";
+import { createDownloadStream } from "../utils";
+
+function concatUnit8Array(chunks: Uint8Array[]) {
+    const len = chunks.reduce((prev, cur) => prev + cur.length, 0);
+    const mergeArr = new Uint8Array(len);
+    chunks.reduce((prev, cur) => {
+        mergeArr.set(cur, prev);
+        return prev + cur.length;
+    }, 0);
+    return mergeArr;
+}
+
+function bufferMacroTaskChunk(cb: (chunk: Uint8Array) => void) {
+    const chunks: Uint8Array[] = [];
+    let flag = false;
+    return (chunk: Uint8Array) => {
+        chunks.push(chunk);
+        if (!flag) {
+            flag = true;
+            setTimeout(() => {
+                console.log('bufferMacroTask start');
+                flag = false;
+                chunks.length = 0;
+                cb(concatUnit8Array(chunks));
+            }, 0);
+        }
+    }
+}
+const onPutDirectory = async (e: any) => {
+    const files: FileList = e.target.files;
+    const streamList: Array<[ReadableStreamDefaultReader, fflate.ZipDeflate]> = [];
+    const writableStream = await createDownloadStream("localFolder.zip");
+    const stream = writableStream.getWriter();
+    let iterator = read();
+    const zip = new fflate.Zip((err: any, data: Uint8Array, final: any) => {
+        if (err || final) { stream.close(); }
+        else {
+            const buffer = bufferMacroTaskChunk((chunk: Uint8Array) => {
+                stream.write(chunk).then(() => iterator.next());
+            })
+            buffer(data);
+        }
+    });
+    async function* read() {
+        for (let i = 0; i < streamList.length; i++) {
+            const [reader, zipStream] = streamList[i];
+            while (true) {
+                console.log('read');
+                const { done, value = new Uint8Array() } = await reader.read();
+                console.log('push', i, done);
+                zipStream.push(value);
+                console.log('yield');
+                yield;
+                console.log('next');
+                if (done) break;
+            }
+        }
+        zip.end();
+    }
+    iterator = read();
+
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files.item(i)!;
+        const zipStream = new fflate.ZipDeflate(file.name, { level: 5 });
+        streamList.push([file.stream().getReader(), zipStream]);
+        zip.add(zipStream);
+    }
+    console.log('start');
+    iterator.next();
+}
+
+function LocalFileZipArea() {
+    const inputRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        inputRef.current?.addEventListener('change', onPutDirectory);
+    }, []);
+    return (<>
+        <button onClick={() => inputRef.current?.click()}>流式-文件压缩</button >
+        <input ref={inputRef} multiple type="file" hidden />
+    </>);
 }
 
 export default LocalFileZipArea
